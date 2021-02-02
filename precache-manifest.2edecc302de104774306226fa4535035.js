1cec785033c7f6ea74ee93b5cb22d2f82464b348
xiaoshengkang <898987943@qq.com>
2021-02-02 19:51:13 +0800

    deploy

---
+++ precache-manifest.2edecc302de104774306226fa4535035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f7112bfc3808938d56",
+    "url": "/morney-website/css/app.332312fe.css"
+  },
+  {
+    "revision": "2a7c4f7fb9a4db4974d76d80ddf54f5b",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "f6f7112bfc3808938d56",
+    "url": "/morney-website/js/app.a506beff.js"
+  },
+  {
+    "revision": "e7f3558b67b9f3fc8d16",
+    "url": "/morney-website/js/chunk-vendors.ff8582d5.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/morney-website/robots.txt"
+  }
+]);
